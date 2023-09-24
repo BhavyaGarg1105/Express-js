e741b2931f883b833ef0d52cf7cf7dbf6e5f3155
BhavyaGarg1105 <bhavyagarg1105@gmail.com>
2023-09-24 20:47:32 +0530

    Learning about Middlewares and Nodemon.

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const customMiddleware = (req,res,next)=>{
+
+};
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./index.html'));
+//   res.json({
+//     "firstName":'Bhavya',
+//     "lastName":'Garg',
+//     "age":22
+// })
+  res.sendFile(path.join(__dirname,'./index.html'));
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World ')
+})
+
+app.get('/hello/:name2', (req, res) => {
+  console.log(req);
+  res.send('Hello World ' + req.params.name2)
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Hello World!')
+    res.send('About Hello World.')
   })
 
 app.listen(port, () => {
